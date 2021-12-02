71bd6e2326a44de8b62f2eca4b5d1e65eb2baa0c
jonpas <jonpas33@gmail.com>
2021-12-02 22:36:41 +0100

    Add day 2 solutions

---
+++ 2021/d2_dive.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+with open("d2_input.txt", "r", encoding="utf-8") as f:
+    data = f.readlines()
+    data = [line.strip().split() for line in data]
+    data = [(move, int(val)) for move, val in data]
+
+position, depth = 0, 0
+
+for move, val in data:
+    if move == "forward":
+        position += val
+    elif move == "down":
+        depth += val
+    elif move == "up":
+        depth -= val
+    else:
+        print(f"error: {move} {val}")
+
+print(f"{position=}, {depth=}")
+
+multiplied = position * depth
+print(f"{multiplied=}")
---
+++ 2021/d2_dive2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+with open("d2_input.txt", "r", encoding="utf-8") as f:
+    data = f.readlines()
+    data = [line.strip().split() for line in data]
+    data = [(move, int(val)) for move, val in data]
+
+position, depth, aim = 0, 0, 0
+
+for move, val in data:
+    if move == "forward":
+        position += val
+        depth += aim * val
+    elif move == "down":
+        aim += val
+    elif move == "up":
+        aim -= val
+    else:
+        print(f"error: {move} {val}")
+
+print(f"{position=}, {depth=}")
+
+multiplied = position * depth
+print(f"{multiplied=}")
